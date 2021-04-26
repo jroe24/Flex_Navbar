0cff32df37ae17fa65f753c209c980a534638c89
Jacob Roe <jroenavy24@gmail.com>
2021-04-26 14:16:02 -0700

    js commit

---
+++ flex_Navbar.js
@@ -0,0 +1,12 @@
+function logout() {
+    console.log("You have succesfully logged out!");
+}
+function myconnections() {
+    console.log("Taking you to My Connections...");
+}
+function myprofile() {
+    console.log("Redirecting you to My Profile page...");
+}
+function home() { 
+    console.log("Taking you to the Home page");
+}
